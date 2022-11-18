d1b1ca07e0b86cbc9a09676587101fe4fe485328
danialmehdizadeh <110994138+danialmehdizadeh@users.noreply.github.com>
2022-11-18 17:51:05 +0330

    Update 2-minesWeeper.js

--- medium-challenges/2-minesWeeper.js
+++ medium-challenges/2-minesWeeper.js
@@ -5,6 +5,7 @@
 // Return an array where each dash is replaced by a digit indicating the number
 //  of mines immediately adjacent to the spot (horizontally, vertically, and diagonally).
 
+
 // Examples
 // numGrid([
 //   ["-", "-", "-", "-", "-"],
@@ -19,6 +20,7 @@
 //   ["0", "1", "1", "1", "0"],
 //   ["0", "0", "0", "0", "0"],
 // ]
+// diagonal x-1 [n - 1,n+1] x+1[n-1,n+1]  x[n-1 n+1]
 
 // numGrid([
 //   ["-", "-", "-", "-", "#"],
@@ -49,3 +51,29 @@
 // ]
 // Notes
 // N/A
+const mineSweeper = (table) => {
+    let [newTable,row,col] = [[...table],0,0];
+    while(row<table.length){
+        while(col<table[row].length){
+            console.log(table[row].length)
+            const cell = checkMine(row,col)
+            newTable[row][col] = cell
+            col++
+        }
+        row++;
+    }
+    return newTable
+}
+const checkMine = () => {
+    return 1
+}
+mineSweeper(
+    [
+          ["1", "1", "2", "#", "#"],
+          ["1", "#", "3", "3", "2"],
+          ["2", "4", "#", "2", "0"],
+          ["1", "#", "#", "2", "0"],
+          ["1", "2", "2", "1", "0"],
+        ]
+)
+//check mine returns number
