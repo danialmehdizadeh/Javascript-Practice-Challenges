05802c91d94127e460b5d829457dc40c00bee0cc
danialmehdizadeh <110994138+danialmehdizadeh@users.noreply.github.com>
2022-11-18 14:51:13 +0330

    solution for 1 - does not work

--- medium-challenges/1-two-distinct-element.js
+++ medium-challenges/1-two-distinct-element.js
@@ -9,3 +9,20 @@
 // returnUnique([9, 5, 6, 8, 7, 7, 1, 1, 1, 1, 1, 9, 8]) ➞ [5, 6]
 // Notes
 // Keep the same ordering in the output.
+let returnUnique = (array) => {
+        let arrayCopy = array.map(element => [element, false]);
+        console.log(arrayCopy)
+        let unique = [];
+        for(let i = 0; i<array.length;i++){
+            arrayCopy[i][1] = true
+            console.log(arrayCopy[i],arrayCopy.includes([array[i], false]))
+            if(arrayCopy.includes([array[i], false])){
+                console.log(array[i] + false)
+                arrayCopy = arrayCopy.filter(x => x[0] === array[i])
+            } else{
+            }
+        }
+        return arrayCopy
+
+}
+console.log(returnUnique([5, 5, 2, 4, 4, 4, 9, 9, 9, 1]))
